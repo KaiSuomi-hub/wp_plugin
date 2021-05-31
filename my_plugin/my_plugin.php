df8e079687471d1cddfa98a98087e1d2e5f63b8d
KaiSuomi-hub <69754493+KaiSuomi-hub@users.noreply.github.com>
2021-05-31 09:05:18 +0300

    first commit

---
+++ my_plugin/my_plugin.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: My plugin
+Plugin URI: http://veppi.fi
+description: >-
+My first plugin
+
+Version: 0.1
+Author: Antti Suomi
+Author URI: Tuotanto@veppi.fi
+License: https://choosealicense.com/licenses/agpl-3.0/
+*/
+?>
